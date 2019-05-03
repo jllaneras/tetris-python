5cb0b928965111c5bd565ede8c6e35e0b424ccf8
Josep Llaneras <jllanerasnadal@adneom.com>
2019-05-03 11:14:12 +0200

    Add help text

--- screen.py
+++ screen.py
@@ -6,11 +6,24 @@ from config import (
     TETRIS_MATRIX_HEIGHT
 )
 
-MATRIX_POS = (1, 1)  # (y, x) position of the Tetrix matrix on the screen
-TETRIS_WIDTH = TETRIS_MATRIX_WIDTH*TetriminoCell.width + 2  # takes the lateral borders into account
-TETRIS_HEIGHT = TETRIS_MATRIX_HEIGHT*TetriminoCell.height + 2  # takes the top/bottom borders into account
-TETRIS_WALL_CHAR = '\u2588'
-SCORE_ROW_COLOR_PAIR = 8
+# (y, x) position of the Tetrix matrix on the screen
+TETRIS_MATRIX_POS = (1, 1)
+
+WALL_CHAR = '\u2588'
+WALL_COLOR_PAIR = curses.COLOR_GREEN
+
+# takes the lateral walls into account
+TETRIS_BOX_WIDTH = TETRIS_MATRIX_WIDTH * TetriminoCell.width + 2 * len(WALL_CHAR)
+
+# takes the top/bottom borders into account
+TETRIS_BOX_HEIGHT = TETRIS_MATRIX_HEIGHT * TetriminoCell.height + 2 * len(WALL_CHAR)
+
+HELP_AREA_WIDTH = 25
+
+GAME_HEIGHT = TETRIS_BOX_HEIGHT
+GAME_WIDTH = TETRIS_BOX_WIDTH + HELP_AREA_WIDTH
+
+SCORE_COLOR_PAIR = 8
 
 class Screen():
 
@@ -19,14 +32,15 @@ class Screen():
 
         self._init_curses(stdscr)
 
-        self._print_tetris_score(stdscr, 0)
-        self._print_lateral_walls(stdscr)
-        self._print_bottom_wall(stdscr)
+        self._render_tetris_score(stdscr, 0)
+        self._render_lateral_walls(stdscr)
+        self._render_bottom_wall(stdscr)
+        self._render_help(stdscr)
 
         stdscr.refresh()
 
     def render_tetris(self, tetris_matrix, score, stdscr):
-        y_offset, x_offset = MATRIX_POS
+        y_offset, x_offset = TETRIS_MATRIX_POS
 
         for y, row in enumerate(tetris_matrix):
             for x, cell in enumerate(row):
@@ -35,9 +49,14 @@ class Screen():
                 else:
                     stdscr.addstr(y_offset + y*TetriminoCell.height, x_offset + x*TetriminoCell.width, ' ' * TetriminoCell.width, curses.color_pair(1))
 
-        self._print_tetris_score(stdscr, score)
+        self._render_tetris_score(stdscr, score)
 
         stdscr.refresh()
+
+    def _assert_window_size(self, stdscr):
+        heigth, width = stdscr.getmaxyx()
+        if heigth < GAME_HEIGHT or width < GAME_WIDTH:
+            raise Exception('The terminal window is too small')
 
     def _init_curses(self, stdscr):
         stdscr.clear()
@@ -54,25 +73,37 @@ class Screen():
         curses.init_pair(curses.COLOR_MAGENTA, curses.COLOR_MAGENTA, curses.COLOR_BLACK)
         curses.init_pair(curses.COLOR_RED, curses.COLOR_RED, curses.COLOR_BLACK)
 
-        curses.init_pair(SCORE_ROW_COLOR_PAIR, curses.COLOR_BLACK, curses.COLOR_GREEN)
+        curses.init_pair(SCORE_COLOR_PAIR, curses.COLOR_BLACK, curses.COLOR_GREEN)
 
-    def _assert_window_size(self, stdscr):
-        heigth, width = stdscr.getmaxyx()
-        if heigth < TETRIS_HEIGHT or width < TETRIS_WIDTH:
-            raise Exception('The terminal window is too small')
-
-    def _print_tetris_score(self, stdscr, score):
+    def _render_tetris_score(self, stdscr, score):
         header = ' Score: {}'.format(score)
-        if len(header) < TETRIS_WIDTH:
-            header = header + (TETRIS_WIDTH - len(header)) * ' '
-        stdscr.addstr(0, 0, header, curses.color_pair(SCORE_ROW_COLOR_PAIR))
+        if len(header) < TETRIS_BOX_WIDTH:
+            header = header + (TETRIS_BOX_WIDTH - len(header)) * ' '
+        stdscr.addstr(0, 0, header, curses.color_pair(SCORE_COLOR_PAIR))
 
-    def _print_lateral_walls(self, stdscr):
-        for y in range(1, TETRIS_HEIGHT):
-            stdscr.addstr(y, 0, TETRIS_WALL_CHAR, curses.color_pair(curses.COLOR_GREEN))
-            stdscr.addstr(y, TETRIS_WIDTH - 1, TETRIS_WALL_CHAR, curses.color_pair(curses.COLOR_GREEN))
+    def _render_lateral_walls(self, stdscr):
+        for y in range(1, GAME_HEIGHT):
+            stdscr.addstr(y, 0, WALL_CHAR, curses.color_pair(WALL_COLOR_PAIR))
+            stdscr.addstr(y, TETRIS_BOX_WIDTH - 1, WALL_CHAR, curses.color_pair(WALL_COLOR_PAIR))
 
-    def _print_bottom_wall(self, stdscr):
-        for x in range(TETRIS_WIDTH):
-            stdscr.addstr(TETRIS_HEIGHT - 1, x, TETRIS_WALL_CHAR, curses.color_pair(curses.COLOR_GREEN))
+    def _render_bottom_wall(self, stdscr):
+        for x in range(TETRIS_BOX_WIDTH):
+            stdscr.addstr(GAME_HEIGHT - 1, x, WALL_CHAR, curses.color_pair(curses.COLOR_GREEN))
 
+    def _render_help(self, stdscr):
+        y = GAME_HEIGHT - 10
+        x = TETRIS_BOX_WIDTH + 2
+        help_lines = [
+            '\u2190 = move left',
+            '\u2192 = move right',
+            '\u2193 = move down',
+            '\u2191 = rotate',
+            '',
+            'Ctrl+S = pause',
+            'Ctrl+Q = continue',
+            '',
+            'Q = exit'
+        ]
+
+        for i, line in enumerate(help_lines):
+            stdscr.addstr(y + i, x, line, curses.color_pair(curses.COLOR_GREEN))
