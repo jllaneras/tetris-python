9dd9508cbd43ef9c5c28f455575f7a679e67fb9f
Josep Llaneras <jllanerasnadal@adneom.com>
2019-05-03 16:23:12 +0200

    Resize the Tetris matrix to the size used in the NES version

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-TETRIS_MATRIX_HEIGHT = 24 
-TETRIS_MATRIX_WIDTH = 16
+TETRIS_MATRIX_HEIGHT = 20
+TETRIS_MATRIX_WIDTH = 10
 
 # Duration of one iteration of the game loop. For example: 1000 ms / 60 = 60 FPS
 TICK_DURATION = 1000/60
